d0ce293abd815f2e65f328b37ccd6c537d87e756
Hannah Edumoh <edumohemmaidiong@gmail.com>
2026-01-31 07:24:31 -0800

    Initial Commit

---
+++ src/main/java/com/smartattendance/smart_attendance_system/SmartAttendanceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.smartattendance.smart_attendance_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAttendanceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartAttendanceSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartattendance/smart_attendance_system/SmartAttendanceSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartattendance.smart_attendance_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAttendanceSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
